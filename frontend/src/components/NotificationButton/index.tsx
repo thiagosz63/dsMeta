02194c1bdf314ed8235332b0a090676135ce2d1d
thiagosz63 <thiagosz63@gmail.com>
2022-07-16 20:02:52 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,20 @@
 import './styles.css'
 import icon from '../../assets/img/notification-icon.svg'
+import axios from 'axios'
+import { BASE_URL } from '../../utils/request'
 
-export default function NotificationButton() {
+type props = {
+    saleId : number
+}
+
+function handleClick(id:number){
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+    .then(()=>console.log("enviado"))
+}
+
+export default function NotificationButton({saleId}:props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
